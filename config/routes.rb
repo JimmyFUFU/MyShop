b581fdd619b39823822dc2374167e46ad6503344
JimmyFUFU <ppp456rrr@gmail.com>
2022-05-30 22:55:55 +0800

    custom registraion and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: redirect('/products')
 
